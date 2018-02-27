06cbe6b1815a08613bfc21f8831d215641b8c025
leonpeery <leonpeery@gmail.com>
2018-02-27 13:30:03 -0500

    changed it gack to price

--- models/item.py
+++ models/item.py
@@ -5,18 +5,18 @@ class ItemModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
-    description = db.Column(db.String(500))
+    price = db.Column(db.Float(precision=2))
 
     store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
     store = db.relationship('StoreModel')
 
-    def __init__(self, name, description, store_id):
+    def __init__(self, name, price, store_id):
         self.name = name
-        self.description = description
+        self.price = price
         self.store_id = store_id
 
     def json(self):
-        return {'name': self.name, 'description': self.description}
+        return {'name': self.name, 'price': self.price}
     
     @classmethod
     def find_by_name(cls, name):
